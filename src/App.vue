08bcc9fcea457dd728d59c5eebb0891e41ec200e
Absurd-Tree <tree@yuexiang.onmicrosoft.com>
2025-10-16 22:11:47 +0800

    del a:href and target

--- src/App.vue
+++ src/App.vue
@@ -51,75 +51,35 @@
     <div class="centent1">
       <!--友情链接-->
       <div class="link_bg">
-        <div class="foot_tlt">友情链接</div>
+        <div class="foot_tlt">合作伙伴</div>
         <div class="link_1">
-          <a href="http://www.ndrc.gov.cn" class="link" target="_blank"
-            >国家发展和改革委员会</a
-          >
-          <a href="http://www.nea.gov.cn/" class="link" target="_blank"
-            >国家能源局</a
-          >
-          <a href="https://fgw.sh.gov.cn/" class="link" target="_blank"
-            >上海市发展和改革委员会</a
-          >
-          <a href="http://ceis.xinhua08.com/" class="link" target="_blank"
-            >中国经济信息社</a
-          >
+          <a class="link">马拉松原油公司</a>
+          <a class="link">道达尔能源公司</a>
+          <a class="link">康菲石油公司‌</a>
+          <a class="link">壳牌集团</a>
         </div>
         <div class="link_2">
-          <a
-            href="http://www.cnpc.com.cn/cnpc/index.shtml"
-            class="link"
-            target="_blank"
-            >中国石油</a
-          >
-          <a href="http://www.sinopec.com" class="link" target="_blank"
-            >中国石化</a
-          >
-          <a href="http://www.cnooc.com.cn/" class="link" target="_blank"
-            >中国海油</a
-          >
-          <a href="https://www.pipechina.com.cn/" class="link" target="_blank"
-            >国家管网</a
-          >
+          <a class="link">中国石油</a>
+          <a class="link">中国石化</a>
+          <a class="link">中国海油</a>
+          <a class="link">国家管网</a>
         </div>
         <div class="link_2">
-          <a href="http://www.shenergy.com.cn/" class="link" target="_blank"
-            >申能集团</a
-          >
-          <a href="http://www.bjgas.com/" class="link" target="_blank"
-            >北京燃气</a
-          >
-          <a href="http://www.towngas.com.cn/" class="link" target="_blank"
-            >港华燃气</a
-          >
-          <a
-            href="https://www.enn.cn/business/energy.html"
-            class="link"
-            target="_blank"
-            >ENN新奥</a
-          >
+          <a class="link">申能集团</a>
+          <a class="link">北京燃气</a>
+          <a class="link">港华燃气</a>
+          <a class="link">ENN新奥</a>
         </div>
         <div class="link_2">
-          <a
-            href="http://www.chinagasholdings.com/"
-            class="link"
-            target="_blank"
-            >中国燃气</a
-          >
-          <a href="https://www.chng.com.cn/" class="link" target="_blank"
-            >中国华能</a
-          >
-          <a href="https://www.greatgas.cn" class="link" target="_blank"
-            >好气网</a
-          >
+          <a class="link">中国燃气</a>
+          <a class="link">中国华能</a>
+          <a class="link">好气网</a>
         </div>
       </div>
       <!--版权所有-->
       <!--  <div class="foot_copyright">©版权所有 2019 上海石油天然气交易中心 沪ICP备15013792号</div> -->
       <div class="foot_copyright">
         <a
-          href="https://www.shpgx.com/html/qtgg/20240806/6640.html"
           style="
             color: #fff;
             font-size: 13px;
@@ -127,17 +87,14 @@
             line-height: 20px;
             margin-right: 20px;
           "
-          target="_blank"
           >法律声明</a
         ><a
-          href="https://beian.miit.gov.cn/"
           style="
             color: #fff;
             font-size: 13px;
             display: inline-block;
             line-height: 20px;
           "
-          target="_blank"
           ><br />©版权所有 2019 上海石油天然气交易中心 沪ICP备15013792号-1</a
         >
       </div>
@@ -187,5 +144,8 @@ watch(
 
 .navigation_tab a:hover {
   text-decoration: none;
+}
+a:hover {
+  cursor: pointer;
 }
 </style>
